e1e1faff19f0a2e7b9bd749fa33fb2935cf94374
MariaJenson96 <mariajenson1996@gmail.com>
2025-04-23 19:55:13 +0530

    ..

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,5 +9,45 @@ import { NgApexchartsModule } from 'ng-apexcharts';
   imports: [IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, NgApexchartsModule],
 })
 export class HomePage {
-  constructor() {}
+  public chartOptions: any;
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: 'Sales',
+          data: [
+            { x: 'Jan', y: 30 },
+            { x: 'Feb', y: 40 },
+            { x: 'Mar', y: 35 },
+            { x: 'Apr', y: 50 },
+            { x: 'May', y: 49 },
+            { x: 'Jun', y: 60 },
+            { x: 'Jul', y: 70 },
+            { x: 'Aug', y: 91 },
+            { x: 'Sep', y: 125 }
+          ]
+        }
+      ],
+      chart: {
+        type: 'area',
+        height: 350,
+        zoom: { enabled: false }
+      },
+      dataLabels: { enabled: false },
+      stroke: {
+        curve: 'smooth',
+        width: 2
+      },
+      title: { text: 'Monthly Sales (Mountain Chart)', align: 'left' },
+      grid: {
+        row: { colors: ['#f3f3f3', 'transparent'], opacity: 0.5 }
+      },
+      xaxis: {
+        type: 'category',
+        categories: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep']
+      },
+      yaxis: { title: { text: 'Units Sold' } }
+    };
+  }
 }
